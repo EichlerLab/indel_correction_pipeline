26a0d2d61efbea3d8b80e97c60e5664ca08c76fa
david <dgordon@uw.edu>
2018-03-15 16:10:06 -0700

    bug fix

--- create_high_low_depth.sh
+++ create_high_low_depth.sh
@@ -1,2 +1,2 @@
 set -v
-awk 'BEGIN {OFS = "\t" } {if ( ($5 < 2.7) || (170 < $5 ) ) print $1, $2, $3, $5 }' /net/eichler/vol18/zevk/great_apes/contig_breaking/clint/depth/merged_smooth/1000/all.smooth.depth.1000.txt >high_low_depth_2.7_170_falcon_coordinates.bed
+awk 'BEGIN {OFS = "\t" } {if ( ($4 < 2.7) || (98.66 < $4 ) ) print $1, $2, $3, $4 }' /net/eichler/vol27/projects/assemblies/nobackups/macaque/read_depth_by_position/merged_smooth/1000/all.smooth.depth.1000.bed >high_low_depth_2.7_99_falcon_coordinates.bed
