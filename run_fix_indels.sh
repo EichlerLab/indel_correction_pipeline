8c009617ddb0e09ec56c1de5e139a6a3c95d09e0
david <dgordon@uw.edu>
2019-06-07 17:29:45 -0700

    run aligning illumina and freebayes and pacbio read depth by position

--- run_fix_indels.sh
+++ run_fix_indels.sh
@@ -1,7 +1,7 @@
 export PATH=.:$PATH
 source source_this_first.sh
-fix_indels_start_earlier.py --szInputGenome /net/eichler/vol27/projects/autism_genome_assembly/nobackups/assemblies/proband/squashed_pilon/squashed_arrowed_piloned.fa \
---szPrefixForSmartie mother190515 \
---nProcessors 22 \
---szIlluminaPiecesForward /net/eichler/vol27/projects/autism_genome_assembly/nobackups/assemblies/proband/illumina_reads/cram_to_fasta/fastqs/pieces_forward \
---szHighAndLowDepthRegionsBed /net/eichler/vol27/projects/autism_genome_assembly/nobackups/assemblies/proband/squashed_indel/read_depth_by_position/high_and_low_depth_regions.bed
+fix_indels_start_earlier.py --szInputGenome /net/eichler/vol27/projects/assemblies/nobackups/marmoset/pilon/marmoset_arrow_pilon.fa \
+--szPrefixForSmartie marm \
+--nProcessors 15 \
+--szIlluminaPiecesForward /net/eichler/vol27/projects/assemblies/nobackups/marmoset/illumina_reads/split/pieces_forward \
+--szPacBioReadsFof /net/eichler/vol27/projects/assemblies/nobackups/marmoset/pacbio_reads/input_pacbio.fofn
