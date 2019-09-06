a89774596ffa0a0b35e888905a6560df26886dba
david <dgordon@uw.edu>
2019-09-06 13:30:09 -0700

    alignIlluminaToFreebayesCorrectedGenome1/source_this_first.sh using miniconda instead of anaconda

--- alignIlluminaToFreebayesCorrectedGenome1/source_this_first.sh
+++ alignIlluminaToFreebayesCorrectedGenome1/source_this_first.sh
@@ -1,4 +1,4 @@
 module purge
-module load modules modules-init modules-gs/prod modules-eichler/prod bedtools/2.21.0 anaconda/201710
+module load modules modules-init modules-gs/prod modules-eichler/prod bedtools/2.21.0 miniconda/4.5.12
 module load bwa/0.7.13
 module load samtools/1.3
