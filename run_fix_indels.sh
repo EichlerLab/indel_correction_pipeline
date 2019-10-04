7a746acf6072fe8ac4f9dced048347148e4ba86f
david <dgordon@uw.edu>
2019-10-04 11:55:38 -0700

    change name from fix_indels_start_earlier.py to fix_indels.py

--- run_fix_indels.sh
+++ run_fix_indels.sh
@@ -1,6 +1,6 @@
 export PATH=.:$PATH
 source source_this_first.sh
-fix_indels_start_earlier.py --szInputGenome /net/eichler/vol27/projects/assemblies/nobackups/marmoset/pilon/marmoset_arrow_pilon.fa \
+fix_indels.py --szInputGenome /net/eichler/vol27/projects/assemblies/nobackups/marmoset/pilon/marmoset_arrow_pilon.fa \
 --szPrefixForSmartie marm \
 --nProcessors 15 \
 --szIlluminaPiecesForward /net/eichler/vol27/projects/assemblies/nobackups/marmoset/illumina_reads/split/pieces_forward \
